4b036552712fe427d60253e90ca287c3369a4f23
root <root@mail.noflag.io>
2017-10-05 18:41:46 +0000

    Fixed typo in init script

--- powerdns/start.sh
+++ powerdns/start.sh
@@ -3,7 +3,7 @@ if [ ! -f /etc/pdns/run/sqlite3/zone_db.sqlite ]; then
 	echo "Creating PowerDNS database..."
 	touch /etc/pdns/run/sqlite3/zone_db.sqlite
 	sqlite3 /etc/pdns/run/sqlite3/zone_db.sqlite < /pdns/create_db.sql
-	if [ "PDNS_TYPE" == "slave" ]; then
+	if [ "$PDNS_TYPE" == "slave" ]; then
 		sqlite3 /etc/pdns/run/sqlite3/zone_db.sqlite 'insert into supermasters values ("$MASTER_IP", "$MASTER_DNS", "admin");'
 		mv /etc/pdns/slave.conf /etc/pdns/pdns.conf
 	fi
@@ -11,11 +11,11 @@ fi
 
 chown pdns:pdns /etc/pdns/run/sqlite3/ -R
 
-if [ "PDNS_TYPE" == "master" ]; then
+if [ "$PDNS_TYPE" == "master" ]; then
 	exec pdns_server --allow-recursion=172.17.0.0/24 --local-address=0.0.0.0 --api-key="$API_KEY" --webserver-password="$WEBSERVER_PASSWORD" "$@"
 fi
 
-if [ "PDNS_TYPE" == "slave" ]; then
+if [ "$PDNS_TYPE" == "slave" ]; then
 	exec pdns_server 	--allow-recursion=172.17.0.0/24 \
 						--local-address=0.0.0.0 \
 						--allow-notify-from=$MASTER_IP \
